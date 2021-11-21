318646131614992a4a377ec41bcd6565bced7238
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2021-11-22 01:24:23 +0300

    Initial commit

---
+++ include/COMiC/os-gui.h
@@ -0,0 +1,17 @@
+#include <COMiC/types.h>
+#include <COMiC/os.h>
+
+#ifndef COMiC_OS_GUI_H
+#define COMiC_OS_GUI_H
+
+#if COMiC_LIMITED
+# pragma pack(push, 1)
+#endif
+
+#include <COMiC/_os-gui.h>
+
+#if COMiC_LIMITED
+# pragma pop()
+#endif
+
+#endif /* COMiC_OS_GUI_H */
